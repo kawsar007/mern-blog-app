88e0854b0bef6f95655c82d621f406c7d15e63ce
kawsar007 <imkawsar007@gmail.com>
2024-05-20 12:29:10 +0600

    Create test API route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -24,3 +25,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000...");
 });
+
+app.use("/api/user", userRoutes);
