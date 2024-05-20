cadb63c48b2b88b8026a9ddf85cd1de2d6548171
kawsar007 <imkawsar007@gmail.com>
2024-05-20 11:40:12 +0600

    Database connection established

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,27 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// var username = encodeURIComponent("imkawsar007");
+// var password = encodeURIComponent("Happy2023?");
+
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error("MONGO_URI environment variable not defined");
+}
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
