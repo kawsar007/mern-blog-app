04918fc854e23c2d849536eaffa9d9535940801e
kawsar007 <imkawsar007@gmail.com>
2024-05-21 18:09:08 +0600

    Signup API Create Successfullay

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 
 // Load environment variables from .env file
@@ -22,8 +23,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000...");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
