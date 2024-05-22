b518f1927305a34e4a469097d12b7f1704685064
kawsar007 <imkawsar007@gmail.com>
2024-05-22 18:18:52 +0600

    Working with signup UI

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp Page</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="">
+        {/* Left */}
+        <div className="left">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Kawsar&apos;s
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
+            nostrum perferendis eligendi cum beatae vitae fugiat repudiandae eos
+            quo fuga!
+          </p>
+        </div>
+
+        {/* Right */}
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
