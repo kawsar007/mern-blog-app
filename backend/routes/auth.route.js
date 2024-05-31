60ebb45be6066c09128793bc7cbae8072bb5868a
kawsar007 <imkawsar007@gmail.com>
2024-05-31 13:02:47 +0600

    Create signin API

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controllar.js";
+import { signin, signup } from "../controllers/auth.controllar.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
