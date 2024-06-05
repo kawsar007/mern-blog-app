9e8031c3cdea2a7a988baf23b907fdbf26dfb021
kawsar007 <imkawsar007@gmail.com>
2024-06-05 22:22:08 +0600

    Working with OAuth

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -105,6 +106,7 @@ const SignUp = () => {
               )}
               ;
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
