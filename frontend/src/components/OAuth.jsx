f479ea36935c46071cb2d11f167a060b8e0d2cab
kawsar007 <imkawsar007@gmail.com>
2024-06-05 22:40:48 +0600

    Create continue with google button comp

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -1,7 +1,20 @@
+import { Button } from "flowbite-react";
 import React from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
 
 const OAuth = () => {
-  return <div>OAuth</div>;
+  const handleGoogleClick = () => {};
+  return (
+    <Button
+      type="button"
+      gradientDuoTone="pinkToOrange"
+      outlone
+      onClick={handleGoogleClick}
+    >
+      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+      Continue with google
+    </Button>
+  );
 };
 
 export default OAuth;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -104,7 +104,6 @@ const SignUp = () => {
               ) : (
                 "Sign Up"
               )}
-              ;
             </Button>
             <OAuth />
           </form>
