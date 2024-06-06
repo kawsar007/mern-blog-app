c466dee72f4e69f5570ac1a2a847f32135ce87c6
kawsar007 <imkawsar007@gmail.com>
2024-06-06 19:06:57 +0600

    install firebase and setup done

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-c88dc.firebaseapp.com",
+  projectId: "mern-blog-c88dc",
+  storageBucket: "mern-blog-c88dc.appspot.com",
+  messagingSenderId: "935962318505",
+  appId: "1:935962318505:web:9e70b4dbbb1785ef187bb8",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
