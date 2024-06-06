7e387b4dcc5af9d8dcca2aee39cb5f8c22ae73de
kawsar007 <imkawsar007@gmail.com>
2024-06-06 19:30:49 +0600

    Firebase google auth done in frontend.

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -1,9 +1,43 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Button } from "flowbite-react";
 import React from "react";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { app } from "../firebase";
+import { signInSuccess } from "../redux/user/userSlice";
 
 const OAuth = () => {
-  const handleGoogleClick = () => {};
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+
+  const handleGoogleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const resultsFromGoogle = await signInWithPopup(auth, provider);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: JSON.stringify({
+            name: resultsFromGoogle.user.displayName,
+            email: resultsFromGoogle.user.email,
+            googlePhotoUrl: resultsFromGoogle.user.photoUrl,
+          }),
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Button
       type="button"
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-c88dc.firebaseapp.com",
   projectId: "mern-blog-c88dc",
   storageBucket: "mern-blog-c88dc.appspot.com",
