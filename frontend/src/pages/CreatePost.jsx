b63260f8671ad2aa53a031fda2f2b980415acf53
kawsar007 <imkawsar007@gmail.com>
2024-07-08 18:12:42 +0600

    Create post UI done

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,5 +1,30 @@
+import { FileInput, Select, TextInput } from "flowbite-react";
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            typr="text"
+            placeholder="Title"
+            required
+            id="title"
+            className="flex-1"
+          />
+          <Select>
+            <option value="uncategorized">Select a category</option>
+            <option value="javascript">Javascript</option>
+            <option value="reactjs">React</option>
+            <option value="nextjs">Next.js</option>
+          </Select>
+        </div>
+        <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
+          <FileInput typr="file" accept="image/*" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
