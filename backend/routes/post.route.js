5d9160e23f1f732ad81c6ed127245913ce435cc1
kawsar007 <imkawsar007@gmail.com>
2024-07-17 23:00:48 +0600

    Get posts issue fix

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,6 +4,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 router.post("/create", verifyToken, create);
-router.get("/post", getPosts);
+router.get("/getposts", getPosts);
 
 export default router;
