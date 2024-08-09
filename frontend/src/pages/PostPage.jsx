99c0ad96baa255ea753b09394f96276c362e6cd6
kawsar007 <imkawsar007@gmail.com>
2024-08-09 22:04:16 +0600

    Complete get single post functionalities

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,6 +1,6 @@
-import { Spinner } from "flowbite-react";
+import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -8,7 +8,7 @@ const PostPage = () => {
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
 
-  console.log(post);
+  console.log(post?.title);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -23,7 +23,7 @@ const PostPage = () => {
           return;
         }
         if (res.ok) {
-          setPost(data.posts);
+          setPost(data.posts[0]);
           setLoading(false);
           setError(false);
         }
@@ -42,7 +42,36 @@ const PostPage = () => {
       </div>
     );
 
-  return <div>PostPage</div>;
+  return (
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {post && post.title}
+      </h1>
+      <Link
+        to={`/search?category=${post.category}`}
+        className="self-center mt-5"
+      >
+        <Button color="gray" pill size="xs">
+          {post && post.category}
+        </Button>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+      />
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className="italic">
+          {post && (post.content.length / 1000).toFixed(0)} mins read
+        </span>
+      </div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+    </main>
+  );
 };
 
 export default PostPage;
