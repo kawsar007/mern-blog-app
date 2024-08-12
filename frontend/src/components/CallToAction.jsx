7923427d3020124adf4cd3999c4b9ac919bc883b
kawsar007 <imkawsar007@gmail.com>
2024-08-12 20:19:45 +0600

    Added my portfolio link and image inside call to action section

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -1,13 +1,16 @@
 import { Button } from "flowbite-react";
+import portfolio from "../assets/portfolio.png";
 
 const CallToAction = () => {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
-        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
-        <p className="text-gray-500 my-2">
+        <h2 className="text-2xl mb-4">
+          You can find more information about me by checking out my portfolio.
+        </h2>
+        {/* <p className="text-gray-500 my-2">
           Checkout these resources with 100 JavaScript Projects
-        </p>
+        </p> */}
         <Button
           gradientDuoTone="purpleToPink"
           className="rounded-tl-xl rounded-bl-none"
@@ -22,7 +25,7 @@ const CallToAction = () => {
         </Button>
       </div>
       <div className="p-7 flex-1">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOP1CXGllbhKmc8_VKUD8_PPpQiFX2oiwjQEd4Q7n1nW7WcMUwLuJWoEQoqxosQMAGHc&usqp=CAU" />
+        <img src={portfolio} alt="Portfolio Image" />
       </div>
     </div>
   );
