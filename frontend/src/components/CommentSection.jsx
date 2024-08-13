1ea95640ac73046e9fe7e399d263e5f60e1fd499
kawsar007 <imkawsar007@gmail.com>
2024-08-13 17:08:46 +0600

    Comment section UI created

--- frontend/src/components/CommentSection.jsx
+++ frontend/src/components/CommentSection.jsx
@@ -1,9 +1,16 @@
 /* eslint-disable react/prop-types */
+import { Button, Textarea } from "flowbite-react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const CommentSection = ({ postId }) => {
   const { currentUser } = useSelector((state) => state.user);
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
@@ -29,6 +36,29 @@ const CommentSection = ({ postId }) => {
             Sign In
           </Link>
         </div>
+      )}
+
+      {currentUser && (
+        <form
+          onSubmit={handleSubmit}
+          className="border border-teal-500 rounded-md p-3"
+        >
+          <Textarea
+            placeholder="Add a comment..."
+            rows="3"
+            maxLength="200"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className="flex justify-between items-center mt-5">
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} characters remaining
+            </p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
+          </div>
+        </form>
       )}
     </div>
   );
