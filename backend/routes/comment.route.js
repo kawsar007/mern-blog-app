47590a6769839ac7252bcf67d60f6df7b47aed99
kawsar007 <imkawsar007@gmail.com>
2024-08-15 16:38:56 +0600

    Comment get API ready

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createComment } from "../controllers/comment.controllar.js";
+import { createComment, getPostComments } from "../controllers/comment.controllar.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
 
 export default router;
