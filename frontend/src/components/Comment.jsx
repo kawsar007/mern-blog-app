928db9d10fa15207fb79ced5dfbcbd74e3ac6d74
kawsar007 <imkawsar007@gmail.com>
2024-08-16 23:01:23 +0600

    Added like functionalities to the comment component.

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
 import moment from "moment";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onLike }) => {
   const [user, setUser] = useState({});
-  console.log(user);
-  
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
-     const getUser = async () => {
+    const getUser = async () => {
       try {
         const res = await fetch(`/api/user/${comment.userId}`);
         const data = await res.json();
@@ -16,25 +17,48 @@ const Comment = ({ comment }) => {
         }
       } catch (error) {
         console.log(error);
-        
       }
-     }
-     getUser();
+    };
+    getUser();
   }, [comment]);
   return (
-    <div className="flex p-4 border-b dark:border-gray-600 text-sm">
-      <div className="flex-shrink-0 mr-3">
-         <img className="w-10 h-10 rounded-full bg-gray-200" src={user?.profilePicture} alt={user?.username} />
+    <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
+      <div className='flex-shrink-0 mr-3'>
+        <img
+          className='w-10 h-10 rounded-full bg-gray-200'
+          src={user?.profilePicture}
+          alt={user?.username}
+        />
       </div>
-      <div className="flex-1">
-        <div className="flex items-center mb-1">
-          <span className="font-bold mr-1 text-xs truncate">{user ? `@${user?.username}` : "anonymous user"}</span>
+      <div className='flex-1'>
+        <div className='flex items-center mb-1'>
+          <span className='font-bold mr-1 text-xs truncate'>
+            {user ? `@${user?.username}` : "anonymous user"}
+          </span>
           <span>{moment(comment?.createdAt).fromNow()}</span>
         </div>
-        <p className="text-gray-500 pb-2">{comment?.content}</p>
+        <p className='text-gray-500 pb-2'>{comment?.content}</p>
+        <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+          <button
+            type='button'
+            className={`text-gray-400 hover:text-blue-500 ${
+              currentUser &&
+              comment.likes.includes(currentUser?._id) &&
+              "!text-blue-500"
+            }`}
+            onClick={() => onLike(comment?._id)}>
+            <FaThumbsUp className='text-sm' />
+          </button>
+          <p className='text-gray-400'>
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    ' ' +
+                    (comment.numberOfLikes === 1 ? 'like' : 'likes')}
+              </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Comment;
