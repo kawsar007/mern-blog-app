488c4cbe7382486f46e4303616b546f581a833b1
kawsar007 <imkawsar007@gmail.com>
2024-11-17 23:30:06 +0600

    Added all social media link

--- frontend/src/components/PersonalInfo/ExploreMe.jsx
+++ frontend/src/components/PersonalInfo/ExploreMe.jsx
@@ -7,10 +7,15 @@ const ExploreMe = () => {
     <div className='w-full rounded-md relative group overflow-hidden'>
       {/*  image  */}
       <img
-        src='https://img.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg?t=st=1728139729~exp=1728143329~hmac=dd0870841ecbe138afdb639fee17206241a94b02b17e1e681ad16eba38f0bd7b&w=996'
+        src='https://scontent.fdac182-1.fna.fbcdn.net/v/t39.30808-6/460733521_3654243751464946_2106644904797238320_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHXbDSg8kJHJ8bYcCmumTDYC8ld7XbETEkLyV3tdsRMSdaN8_Xff0LHP0Qkl8LjXWNoYQwe0lnnfMsGhanVtGUi&_nc_ohc=tNlPyoJC4FUQ7kNvgHIvlYy&_nc_zt=23&_nc_ht=scontent.fdac182-1.fna&_nc_gid=AUYjuUS7RSUL0H-qfckUjbj&oh=00_AYD8twlc3vAPVTEgre-WbKZHmUv-ilx3vW6nGMo6CEUOrw&oe=673FDE27'
         alt='animated_cards'
         className='w-full h-[450px] object-cover'
       />
+      {/* <img
+        src='https://img.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg?t=st=1728139729~exp=1728143329~hmac=dd0870841ecbe138afdb639fee17206241a94b02b17e1e681ad16eba38f0bd7b&w=996'
+        alt='animated_cards'
+        className='w-full h-[450px] object-cover'
+      /> */}
 
       {/*  texts  */}
       <div className='flex items-center justify-evenly backdrop-blur-md text-white absolute bottom-0 w-full pt-[15px] pb-[30px] translate-y-[200px] group-hover:translate-y-0 transition-all duration-[400ms] overflow-hidden'>
--- frontend/src/components/PersonalInfo/ProfileCard.jsx
+++ frontend/src/components/PersonalInfo/ProfileCard.jsx
@@ -6,7 +6,7 @@ import {
   FaGithub,
   FaInstagram,
   FaLinkedin,
-  FaTwitter
+  FaTwitter,
 } from "react-icons/fa";
 import { IoIosSend } from "react-icons/io";
 import { Link } from "react-router-dom";
@@ -38,62 +38,55 @@ const ProfileCard = () => {
       </div>
 
       <div className='mt-3 mb-2 px-2 flex gap-14 md:!gap-14'>
-        I&apos;m a passionate Full Stack Developer with expertise in React, Next,
-        Vue, TypeScript, JavaScript, Node, Express, MongoDB and Python.
+        I&apos;m a passionate Full Stack Developer with expertise in React,
+        Next, Vue, TypeScript, JavaScript, Node, Express, MongoDB and Python.
       </div>
 
       <div className='mt-6 flex justify-center gap-4 mb-4'>
         <button className='rounded-lg bg-green-700 px-4 py-1 text-white transition hover:bg-green-600 flex justify-center items-center'>
-          Contact Me <IoIosSend className="h-4 w-4 ml-2" />
+          Contact Me <IoIosSend className='h-4 w-4 ml-2' />
         </button>
         <button className='rounded-full bg-blue-600 p-3 text-white transition hover:bg-blue-700'>
           <FaFacebookMessenger className='w-5 h-5' />
         </button>
       </div>
 
-      {/* Stats */}
-      {/* <div className='mt-6 mb-3 flex gap-14 md:!gap-14'>
-        <div className='flex flex-col items-center justify-center'>
-          <p className='text-2xl font-bold text-navy-700 dark:text-white'>17</p>
-          <p className='text-sm font-normal text-gray-600'>Posts</p>
-        </div>
-        <div className='flex flex-col items-center justify-center'>
-          <p className='text-2xl font-bold text-navy-700 dark:text-white'>
-            9.7K
-          </p>
-          <p className='text-sm font-normal text-gray-600'>Followers</p>
-        </div>
-        <div className='flex flex-col items-center justify-center'>
-          <p className='text-2xl font-bold text-navy-700 dark:text-white'>
-            434
-          </p>
-          <p className='text-sm font-normal text-gray-600'>Following</p>
-        </div>
-      </div> */}
-
       <div className='mt-2 w-full border-t-2 border-black dark:border-white'>
         <div className='mt-2 flex justify-center gap-6 pb-2 text-gray-400'>
-          <Link href='#' className='transition hover:text-gray-200'>
+          <Link
+            href='imkawsar007@gmail.com'
+            className='transition hover:text-gray-200'>
             <CiMail className='w-5 h-5' />
           </Link>
-          <Link href='#' className='transition hover:text-gray-200'>
+          <Link
+            to='https://www.facebook.com/imkawsar007'
+            target='_blank'
+            className='transition hover:text-gray-200'>
             <FaFacebook className='w-5 h-5' />
           </Link>
-          <Link href='#' className='transition hover:text-gray-200'>
+          <Link
+            to='https://github.com/kawsar007'
+            target='_blank'
+            className='transition hover:text-gray-200'>
             <FaGithub className='w-5 h-5' />
           </Link>
           <Link href='#' className='transition hover:text-gray-200'>
             <FaTwitter className='w-5 h-5' />
           </Link>
 
-          <Link href='#' className='transition hover:text-gray-200'>
+          <Link
+            to='https://www.linkedin.com/in/kawsar007/'
+            target='_blank'
+            className='transition hover:text-gray-200'>
             <FaLinkedin className='w-5 h-5' />
           </Link>
           <Link href='#' className='transition hover:text-gray-200'>
             <FaInstagram className='w-5 h-5' />
           </Link>
-          <Link href='#' className='transition hover:text-gray-200'>
-          <FaDribbble className='w-5 h-5' />
+          <Link
+            to='https://kawsar-mia.netlify.app/'
+            className='transition hover:text-gray-200'>
+            <FaDribbble className='w-5 h-5' />
           </Link>
         </div>
       </div>
